eb8e2dbfa91a6e5d66f9b08391ff63f4500e7fd1
Paul Miller <pmiller@microsoft.com>
2025-01-23 06:37:56 +0000

    goldpinger

---
+++ goldpinger.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://gist.githubusercontent.com/paulgmiller/084bd4605f1661a329e5ab891a826ae0/raw/94a32d259e137bb300ac8af3ef71caa471463f23/goldpinger-daemon.yaml
+kubectl apply -f https://gist.githubusercontent.com/paulgmiller/7bca68cd08cccb4e9bc72b0a08485edf/raw/d6a103fb79a65083f6555e4d822554ed64f510f8/goldpinger-deploy.yaml
